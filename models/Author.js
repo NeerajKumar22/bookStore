eeeb44a35c8ced83ae5d4a2cc046c4ab40b6d668
Neeraj Kumar <ineerajkr@gmail.com>
2019-06-11 22:53:58 +0530

    add Author schema file

---
+++ models/Author.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var authorSchema = new Schema({
+    name : {
+        type : "String",
+        required : [
+            true,
+            "Name is required",
+        ]
+    },
+    email : {
+        type : "String",
+        required : [
+            true,
+            "Email is required",
+        ],
+        unique : true
+    },
+    age : {
+        type : Number
+    },
+    books : [{type : Schema.Types.ObjectId, ref : "Book"}]
+
+}, {timestamps : true});
+
+module.exports = mongoose.model(("Author", authorSchema));
