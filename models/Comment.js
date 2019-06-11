3183e6c61069f8be37849a243613537f244e7967
Neeraj Kumar <ineerajkr@gmail.com>
2019-06-11 22:55:46 +0530

    add comment schema file

---
+++ models/Comment.js
@@ -0,0 +1,25 @@
+var mongoose = require ("mongoose");
+
+var Schema = mongoose.Schema;
+
+var commentSchema = new Schema({
+    comment : {
+        type : "String",
+        required : [
+            true,
+            "Comment is required"
+        ]
+    },
+
+    book : 
+        {
+            type : Schema.Types.ObjectId,
+            ref : "Book",
+            required : [
+                true,
+                "Book is required"
+            ]
+        },
+}, {timestamps : true});
+
+module.exports = mongoose.model("Comment", commentSchema);
