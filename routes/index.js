412fd4f60b34a9a55e87c1684bbd8d71d2f779ba
Neeraj Kumar <ineerajkr@gmail.com>
2019-06-11 22:43:48 +0530

    add index route file

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var auth = require('../utils/auth');
+
+/* GET home page. */
+router.get('/', auth.verifyToken, function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
