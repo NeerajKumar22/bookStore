cc02a05707435f8573c733dd795a61f0fd7ed4b7
Neeraj Kumar <ineerajkr@gmail.com>
2019-06-11 23:07:33 +0530

    add client/src/index.js file

---
+++ client/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Books from './Books';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+function Main(){
+    return (
+        <BrowserRouter>
+            <Route exact path="/" component={App} />
+            <Route path="/books" component={Books} />
+        </BrowserRouter>
+    );
+};
+
+ReactDOM.render(<Main />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
